dc8e711ad5f12817cb7b415c7b0547f666cf68b7
soda <atsushi037088@gmail.com>
2020-11-24 00:18:31 +0900

    first commit

---
+++ gui/Viewer.java
@@ -0,0 +1,76 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import logic.Dealer;
+
+public class Viewer extends JFrame implements ActionListener{
+	JLabel result;
+
+	JTextField[] tf=new JTextField[13];
+	JTextField times;
+
+	public static void main(String[] args) {
+		Viewer frame = new Viewer("BLACK JACK シミュレーター");
+		frame.setVisible(true);
+	}
+
+	Viewer(String title){
+		setTitle(title);
+		setBounds(100, 200, 500, 500);
+		//閉じるボタン追加
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JButton button = new JButton("計算開始");
+	    button.addActionListener(this);JPanel p = new JPanel();
+	    p.setLayout(new GridLayout(8,10));
+
+	    //トランプの枚数入力フォーム作成
+	    for(int i=0;i<13;i++){
+            tf[i]=new JTextField("24",3);
+            p.add(new JLabel(i+1+"の残り枚数:", SwingConstants.LEFT));
+            p.add(tf[i]);
+        }
+	    p.add(new JLabel("ドロー回数", SwingConstants.LEFT));
+	    times = new JTextField("100000",10);
+	    p.add(times);
+	    p.add(button);
+
+	    JLabel label1 = new JLabel("山札のトランプの残り枚数を入力して下さい。");
+	    result = new JLabel();
+
+	    Container contentPane = getContentPane();
+	    contentPane.add(label1, BorderLayout.NORTH);
+	    contentPane.add(p, BorderLayout.SOUTH);
+	    contentPane.add(result, BorderLayout.CENTER);
+	}
+
+	public void actionPerformed(ActionEvent e){
+		int[] cardList= new int[13];
+		for(int i=0;i<13;i++) {
+			cardList[i]=Integer.parseInt(tf[i].getText());
+		}
+		Dealer dl = new Dealer(cardList);
+		dl.operate(Integer.parseInt(times.getText()));
+		//System.out.println(dl.scoreList);
+		result.setText("<html><body>ゲーム回数 : " + String.valueOf(dl.scoreList.size()) + " 回" + "<br/>"+ "<br/>"
+		+ "21 出現回数： " + String.valueOf(dl.scoreCount[21]) + " 回"+"　　　確率： <font color=\"#ff4500\">"+(String.format("%.2f", (double)dl.scoreCount[21]/dl.scoreList.size()*100))+" % </font>"+"<br/>"
+		+ "20 出現回数： " + String.valueOf(dl.scoreCount[20]) + " 回"+"　　　確率： <font color=\"#ff4500\">"+(String.format("%.2f", (double)dl.scoreCount[20]/dl.scoreList.size()*100))+" % </font>"+"<br/>"
+		+ "19 出現回数： " + String.valueOf(dl.scoreCount[19]) + " 回"+"　　　確率： <font color=\"#ff4500\">"+(String.format("%.2f", (double)dl.scoreCount[19]/dl.scoreList.size()*100))+" % </font>"+"<br/>"
+		+ "18 出現回数： " + String.valueOf(dl.scoreCount[18]) + " 回"+"　　　確率： <font color=\"#ff4500\">"+(String.format("%.2f", (double)dl.scoreCount[18]/dl.scoreList.size()*100))+" % </font>"+"<br/>"
+		+ "17 出現回数： " + String.valueOf(dl.scoreCount[17]) + " 回"+"　　　確率： <font color=\"#ff4500\">"+(String.format("%.2f", (double)dl.scoreCount[17]/dl.scoreList.size()*100))+" % </font>"+"<br/>"
+		+ "バースト回数： " + String.valueOf(dl.scoreCount[0]) + " 回"+"　　　確率： <font color=\"#ff4500\">"+(String.format("%.2f", (double)dl.scoreCount[0]/dl.scoreList.size()*100))+" % </font>"+"<br/>"
+				+ "</body></html>");
+	}
+}
---
+++ logic/Dealer.java
@@ -0,0 +1,90 @@
+/**
+ *ディーラー処理BL
+ */
+package logic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Dealer {
+	//クラス変数
+	public int[] cardList;
+	public int score = 0;
+	public ArrayList<Integer> cardPool;
+	public ArrayList<Integer> scoreList = new ArrayList<Integer>();
+	public int[] scoreCount = new int[22];//0から21までの添え字
+
+	//コンストラクター
+	public Dealer(int[] cardList) {
+		super();
+		this.cardList = cardList;
+		this.cardPool = new ArrayList<Integer>() {{
+			for (int i=0;i<=12;i++) {
+				for (int j=0;j<cardList[i];j++) {
+					//絵柄のトランプは10として扱う
+					if(i<10) {
+						add(Integer.valueOf(i+1));
+					}else {
+						add(Integer.valueOf(10));
+					}
+				}
+		    }
+		}};
+	}
+	//最終スコア集計メソッド
+	public void scoreAggregate (int score) {
+		scoreList.add(score);
+		scoreCount[score]= scoreCount[score] + 1;
+	}
+
+	//一定回数 (count) カードを引き、スコア計算
+	//scoreList 配列に結果を格納
+	public void operate(int count) {
+		int score =0;
+		int k = 0;
+		//Aを引いたかチェック
+		boolean A_flg = false;
+		//一回目シャッフル
+		Collections.shuffle(cardPool);
+		for (int i=0;i<count;i++) {
+			//System.out.println(cardPool.get(k));
+			//Aを引いた場合は、さらに条件分岐
+			if(cardPool.get(k)==1) {
+				//1を足して17～21になる場合
+				A_flg = true;
+			    score = score + 11;
+			}else {
+			//A以外を引いた場合は、普通に合計
+				score = score + cardPool.get(k);
+			}
+			k=k+1;
+
+			//バーストの条件分岐
+			if(score >21) {
+				//Aを引いていてバーストした場合、11を1として再計算する。
+				if(A_flg) {
+					score = score -10;
+					A_flg = false;
+				}else {
+					//バーストの場合、最終スコアを0とする
+					scoreAggregate(0);
+					Collections.shuffle(cardPool);
+					//初期化処理
+					score = 0;
+					k=0;
+					A_flg = false;
+				}
+			}
+			//17～21になった時点で終了
+			if(17 <= score && score <= 21) {
+				scoreAggregate(score);
+				Collections.shuffle(cardPool);
+				//初期化処理
+				score = 0;
+				k=0;
+				A_flg = false;
+			}
+		}
+	}
+
+}
